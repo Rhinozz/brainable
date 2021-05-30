cbde3a63a4e3b1f4fa38dd4ad788b147dda870b5
Rhinozz <68926744+Rhinozz@users.noreply.github.com>
2021-05-30 18:17:08 -0400

    Fix typo in comment

--- Brainable.js
+++ Brainable.js
@@ -3,7 +3,7 @@
 // icon-color: deep-blue; icon-glyph: hdd;
 /*
 A brainf*** interpreter in Scriptable.
-Code almost entirely from Shawn McLaughlin's inrerpreter - https://gist.github.com/shawnmcla/9a6f98fe4f176b99fe1720b3bc23f005
+Code almost entirely from Shawn McLaughlin's interpreter - https://gist.github.com/shawnmcla/9a6f98fe4f176b99fe1720b3bc23f005
 
 Forked by Rhinozz into Brainable.
 
